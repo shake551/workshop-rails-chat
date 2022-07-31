d495a28e141f34bed719a31791f75948f8abd58a
shake551 <hiroki.yamako@ezweb.ne.jp>
2022-07-31 07:04:42 +0000

    add post process

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,5 +3,16 @@ class ChatController < ApplicationController
   end
 
   def create
+    m = Message.new
+    m.name = params[:name]
+    m.message = params[:message]
+
+    if m.valid? && m.save
+      redirect_to action: :index
+      return
   end
+
+  flash[:errors] = m.errors.full_messages
+  render :create, status: :unprocessable_entry
 end
+
